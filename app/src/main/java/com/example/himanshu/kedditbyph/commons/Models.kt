9222049fc48bbb7ecc442899e22a7b4c1cc8c532
HimanshuHP <him.phogat123@gmail.com>
2017-05-28 21:27:32 +0530

    viewtype interface implemented in news item model

--- app/src/main/java/com/example/himanshu/kedditbyph/commons/Models.kt
+++ app/src/main/java/com/example/himanshu/kedditbyph/commons/Models.kt
@@ -1,11 +1,14 @@
 package com.example.himanshu.kedditbyph.commons
 
+import com.example.himanshu.kedditbyph.commons.adapter.AdapterConstants
+import com.example.himanshu.kedditbyph.commons.adapter.ViewType
+
 /**
  * Created by himanshu on 28/05/17.
  */
-data class RedditNewsItem(val author: String,
-                          val title: String,
-                          val numComments: Int,
-                          val created: Long,
-                          val thumbnail: String,
-                          val url: String)
+data class RedditNewsItem(val author: String, val title: String,
+                          val numComments: Int, val created: Long,
+                          val thumbnail: String, val url: String) : ViewType {
+    
+    override fun getViewType() = AdapterConstants.NEWS
+}
