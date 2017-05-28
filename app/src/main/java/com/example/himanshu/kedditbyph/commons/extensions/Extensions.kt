d5d69895d7f5fd3c7de4d8fe89a8744c926536f0
HimanshuHP <him.phogat123@gmail.com>
2017-05-28 22:30:49 +0530

    newsdelegateadapter added

--- app/src/main/java/com/example/himanshu/kedditbyph/commons/extensions/Extensions.kt
+++ app/src/main/java/com/example/himanshu/kedditbyph/commons/extensions/Extensions.kt
@@ -2,9 +2,13 @@
 
 package com.example.himanshu.kedditbyph.commons.extensions
 
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.himanshu.kedditbyph.R
+import com.squareup.picasso.Picasso
 
 /**
  * Created by himanshu on 28/05/17.
@@ -12,4 +16,11 @@ import android.view.ViewGroup
 
 fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+}
+
+fun ImageView.loadImg(imageUrl: String) {
+    if (TextUtils.isEmpty(imageUrl))
+        Picasso.with(context).load(R.mipmap.ic_launcher).into(this)
+    else
+        Picasso.with(context).load(imageUrl).into(this)
 }
