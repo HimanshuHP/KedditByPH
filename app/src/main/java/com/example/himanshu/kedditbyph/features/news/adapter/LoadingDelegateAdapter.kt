9ece179ba52e4ba133b9d16efc0ec1cf792ef5ba
HimanshuHP <him.phogat123@gmail.com>
2017-05-29 15:16:39 +0530

    temp commit

--- app/src/main/java/com/example/himanshu/kedditbyph/features/news/adapter/LoadingDelegateAdapter.kt
+++ app/src/main/java/com/example/himanshu/kedditbyph/features/news/adapter/LoadingDelegateAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.himanshu.kedditbyph.features.news.adapter
 
+import android.support.v7.widget.RecyclerView
+import com.example.himanshu.kedditbyph.R
 import com.example.himanshu.kedditbyph.commons.extensions.inflate
 
 /**
@@ -7,12 +9,12 @@ import com.example.himanshu.kedditbyph.commons.extensions.inflate
  */
 class LoadingDelegateAdapter : com.example.himanshu.kedditbyph.commons.adapter.ViewTypeDelegateAdapter {
 
-    override fun onCreateViewHolder(parent: android.view.ViewGroup): android.support.v7.widget.RecyclerView.ViewHolder = com.example.himanshu.kedditbyph.features.news.adapter.LoadingDelegateAdapter.LoadingViewHolder(parent)
+    override fun onCreateViewHolder(parent: android.view.ViewGroup): RecyclerView.ViewHolder = LoadingViewHolder(parent)
 
-    override fun onBindViewHolder(holder: android.support.v7.widget.RecyclerView.ViewHolder, item: com.example.himanshu.kedditbyph.commons.adapter.ViewType) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: com.example.himanshu.kedditbyph.commons.adapter.ViewType) {
     }
 
-    class LoadingViewHolder(parent : android.view.ViewGroup) : android.support.v7.widget.RecyclerView.ViewHolder(parent.inflate(com.example.himanshu.kedditbyph.R.layout.news_item_loading)){
+    class LoadingViewHolder(parent : android.view.ViewGroup) : RecyclerView.ViewHolder(parent.inflate(R.layout.news_item_loading)){
 
     }
 }
--- app/src/main/java/com/example/himanshu/kedditbyph/features/news/adapter/NewsDelegateAdapter.kt
+++ app/src/main/java/com/example/himanshu/kedditbyph/features/news/adapter/NewsDelegateAdapter.kt
@@ -1,28 +1,31 @@
 package com.example.himanshu.kedditbyph.features.news.adapter
 
+import android.support.v7.widget.RecyclerView
 import com.example.himanshu.kedditbyph.commons.extensions.getFriendlyTime
 import com.example.himanshu.kedditbyph.commons.extensions.inflate
+import com.example.himanshu.kedditbyph.commons.extensions.loadImg
+import kotlinx.android.synthetic.main.news_item.view.*
 
 /**
  * Created by himanshu on 28/05/17.
  */
 class NewsDelegateAdapter : com.example.himanshu.kedditbyph.commons.adapter.ViewTypeDelegateAdapter {
-    override fun onCreateViewHolder(parent: android.view.ViewGroup): android.support.v7.widget.RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: android.view.ViewGroup): RecyclerView.ViewHolder {
         return com.example.himanshu.kedditbyph.features.news.adapter.NewsDelegateAdapter.NewsViewHolder(parent)
     }
 
-    override fun onBindViewHolder(holder: android.support.v7.widget.RecyclerView.ViewHolder, item: com.example.himanshu.kedditbyph.commons.adapter.ViewType) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: com.example.himanshu.kedditbyph.commons.adapter.ViewType) {
         holder as com.example.himanshu.kedditbyph.features.news.adapter.NewsDelegateAdapter.NewsViewHolder
         holder.bind(item as com.example.himanshu.kedditbyph.commons.RedditNewsItem)
     }
 
-    class NewsViewHolder(parent: android.view.ViewGroup) : android.support.v7.widget.RecyclerView.ViewHolder(parent.inflate(com.example.himanshu.kedditbyph.R.layout.news_item)) {
+    class NewsViewHolder(parent: android.view.ViewGroup) : RecyclerView.ViewHolder(parent.inflate(com.example.himanshu.kedditbyph.R.layout.news_item)) {
         fun bind(item: com.example.himanshu.kedditbyph.commons.RedditNewsItem) = with(itemView) {
-            kotlinx.android.synthetic.main.news_item.view.img_thumbnail.loadImg(item.thumbnail)
-            kotlinx.android.synthetic.main.news_item.view.description.text = item.title
-            kotlinx.android.synthetic.main.news_item.view.author.text = item.author
-            kotlinx.android.synthetic.main.news_item.view.comments.text = "${item.numComments} comments"
-            kotlinx.android.synthetic.main.news_item.view.time.text = item.created.getFriendlyTime()
+            img_thumbnail.loadImg(item.thumbnail)
+            description.text = item.title
+            author.text = item.author
+            comments.text = "${item.numComments} comments"
+            time.text = item.created.getFriendlyTime()
 
         }
 
